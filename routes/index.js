e0a2d078be8d379ec42ca678fac84b1a1d4b6072
kakoff17 <carloskako2@gmail.com>
2023-05-07 20:20:20 +0200

    Done but my DB is not working, tried 100 different things

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,34 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
+const Movies = require("../models/Movie.model.js");
+
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get("/", (req, res, next) => res.render("index"));
+
+router.get("/movies", (req, res, next) => {
+  Movies.find()
+    .then((response) => {
+      res.render("../views/movies/movies.hbs", {
+        allMovies: response,
+      });
+    })
+    .catch((error) => {
+      next(error);
+    })
+})
+
+router.get("/movie/:id", (req, res, next) => {
+  Movies.findById(req.params.id)
+    .then((response) => {
+      console.log("Pelicula por ID");
+      res.render("../views/movies/movie.hbs", {
+        movie: response,
+      });
+    })
+    .catch((error) => {
+      next(error);
+    })
+})
 
 module.exports = router;
